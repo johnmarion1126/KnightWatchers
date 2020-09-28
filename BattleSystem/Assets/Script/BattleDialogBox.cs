826993fc62d48b2c2a3f13eecf5d506f40fbfd7a
John Sanchez <johnmarion1126@gmail.com>
2020-09-28 00:02:53 -0700

    adding initial battle system

---
+++ BattleSystem/Assets/Script/BattleDialogBox.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleDialogBox : MonoBehaviour
+{
+    [SerializeField]
+    int lettersPerSecond;
+    [SerializeField]
+    Color highlightedColor;
+
+    [SerializeField]
+    Text dialogText;
+    [SerializeField]
+    GameObject actionSelector;
+
+    [SerializeField]
+    List<Text> actionTexts;
+
+    public void SetDialog(string dialog)
+    {
+        dialogText.text = dialog;    
+    }
+
+    public IEnumerator TypeDialog(string dialog)
+    {
+        dialogText.text = "";
+        foreach (var letter in dialog.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f/lettersPerSecond);
+        }
+    }
+
+    public void EnableDialogText(bool enabled)
+    {
+        dialogText.enabled = enabled;
+    }
+
+    public void EnableActionSelector(bool enabled)
+    {
+        actionSelector.SetActive(enabled);
+    }
+
+    public void UpdateActionSelection(int selectedAction)
+    {
+        for (int i = 0; i < actionTexts.Count; ++i)
+        {
+            if (i == selectedAction)
+            {
+                actionTexts[i].color = highlightedColor;
+            }
+            else
+            {
+                actionTexts[i].color = Color.black;
+            }
+        }
+    }
+}
---
+++ BattleSystem/Assets/Script/HPBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBar : MonoBehaviour
+{
+    [SerializeField]
+    GameObject health;
+
+    private float currentHealth = 1f;
+    
+    void Start()
+    {
+        health.transform.localScale = new Vector3(currentHealth,1f);
+    }
+
+    public void takeDamage(float damage)
+    {
+        health.transform.localScale = new Vector3(currentHealth-damage,1f);
+    }
+}
