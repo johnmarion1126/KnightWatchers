38fb016476ef924a3545a7ea195dafae17493944
marisa-r15 <marisa.ramey15@gmail.com>
2020-10-19 20:10:12 -0700

    recommit

---
+++ BattleSystem/Assets/Script/BattleCollide.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleCollide : MonoBehaviour
+{
+    public string sceneToLoad = "BattleScene";
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+        Debug.Log("GameObject2 collided with " + col.name);
+    }
+}
---
+++ BattleSystem/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+public float speed;
+private Rigidbody2D heroRigidbody;
+private Vector3 change;
+private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         animator = GetComponent<Animator>(); 
+         heroRigidbody = GetComponent<Rigidbody2D>();
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+       
+           
+    }
+
+    void UpdateAnimationAndMove()
+    { if(change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("MoveX", change.x);
+            animator.SetFloat("MoveY", change.y);
+            animator.SetBool("Moving", true);
+            }else{animator.SetBool("Moving",false);}
+    }
+
+    void MoveCharacter()
+    {
+        heroRigidbody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+        );
+    }
+}
