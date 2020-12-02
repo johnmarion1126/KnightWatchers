4293f32df0a092555eb2fd77b4daadb29ff502f8
John Sanchez <johnmarion1126@gmail.com>
2020-12-02 00:05:12 -0800

    adding start scene and end scene

---
+++ BattleSystem/Assets/Script/SceneTransitions/ClickToContinue.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ClickToContinue : MonoBehaviour
+{
+    public string scene;
+    public Image whiteFade;
+    
+    void Start()
+    {
+        whiteFade.canvasRenderer.SetAlpha(0.0f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            whiteFade.CrossFadeAlpha(1,2,false);
+            Invoke("changeScene",2);
+        }
+    }
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
---
+++ BattleSystem/Assets/Script/SceneTransitions/SceneTransition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public string sceneToLoad;
+    public Image whiteFade;
+
+    void Start()
+    {
+        whiteFade.canvasRenderer.SetAlpha(0.0f);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {   
+        whiteFade.CrossFadeAlpha(1,2,false);
+        if(other.CompareTag("Player") && !other.isTrigger)
+        {   
+           Invoke("changeScene",2);
+        }
+    }
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
