eb78fc590758510d58a638b2e3a177ff361ab31a
marisa-r15 <marisa.ramey15@gmail.com>
2020-12-04 17:04:49 -0800

    no message

--- BattleSystem/Assets/Script/StorySection/Story4Continue2.cs
+++ BattleSystem/Assets/Script/StorySection/Story4Continue2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Story4Continue : MonoBehaviour
+public class Story4Continue2 : MonoBehaviour
 {
     public void ContinueButton()
     {
