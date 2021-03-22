a130ef9c70542193e2256f5c78d6cf6fb894a0c5
John Sanchez <johnmarion1126@gmail.com>
2021-03-22 15:18:36 -0700

    addding images of game

---
+++ BattleSystem/Assets/Script/ReadSign1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadSign1 : MonoBehaviour
+{
+    public GameObject Gorgon;
+    public string text;
+
+    public void OnTriggerStay2D(Collider2D Gorgon) 
+    {
+        Debug.Log(text);
+    }
+}
